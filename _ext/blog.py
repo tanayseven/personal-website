e5d122641cf38df13d279489a55ba9b5fd5d8bb6
Tanay PrabhuDesai <tanayseven@gmail.com>
2022-01-05 20:18:02 -0500

    Show all blogs via the all-posts directive

--- _ext/blog.py
+++ _ext/blog.py
@@ -1,35 +1,33 @@
 import re
 from datetime import datetime
-from typing import List, Tuple, Dict
+from typing import TypedDict, Optional
 
 from docutils import nodes
-from docutils.nodes import Element, TextElement
 from docutils.parsers.rst import Directive
-from sphinx.addnodes import pending_xref
+from sphinx.util.docutils import SphinxDirective
 from sphinx.application import Sphinx
 from sphinx.directives import Node
-from sphinx.domains import Domain
-from sphinx.roles import XRefRole
+from sphinx.environment import BuildEnvironment
 from sphinx.util.nodes import set_source_info
 from sphinx.writers.html5 import HTML5Translator
 from sphinx.writers.texinfo import TexinfoTranslator
 from sphinx.writers.text import TextTranslator
 
 
-class PostNode(nodes.General, nodes.Element):
-    name = "PostNode"
+class PostNode(nodes.Element):
+    ...
 
 
 def visit_post_node_html(translator: HTML5Translator, node: PostNode):
-    translator.visit_section(node)
+    ...
 
 
 def depart_post_node_html(translator: HTML5Translator, node: PostNode):
-    translator.depart_section(node)
+    ...
 
 
 def visit_post_node_texinfo(translator: TexinfoTranslator, node: PostNode):
-    translator.visit_section(node)
+    ...
 
 
 def depart_post_node_texinfo(translator: TexinfoTranslator, node: PostNode):
@@ -53,7 +51,7 @@ def _fetch_date_from_file(node):
     return date
 
 
-class PostDirective(Directive):
+class PostDirective(SphinxDirective):
     has_content = True
 
     def run(self) -> list[Node]:
@@ -64,54 +62,139 @@ class PostDirective(Directive):
         node.append(
             nodes.line(text=f"Published on: {date.strftime('%d %B %Y')}"),
         )
-        node.append(
-            nodes.transition()
+        node.append(nodes.transition())
+        titles = [x for x in node.document.traverse() if hasattr(x, "title")]
+        title = titles[0].astext() if titles else None
+        self.state.nested_parse(self.content, self.content_offset, node)
+        # add posts to all posts
+        if not hasattr(self.env, "all_posts"):
+            self.env.all_posts = []
+        target_id = "post-%d" % self.env.new_serialno("post")
+        target_node = nodes.target("", "", ids=[target_id])
+        self.env.all_posts.append(
+            {
+                "title": title,
+                "docname": self.env.docname,
+                "target": target_node,
+                "date": date,
+                "node": node,
+            }
         )
-        return [node]
+        return [target_node, node]
 
 
-class AllPostsNode(TextElement):
-    pass
+class AllPostsNode(nodes.General, nodes.Element):
+    ...
 
 
-class Posts(Domain):
-    name = "posts"
-    label = "List of all the posts"
-
-    roles = {
-        'reref': XRefRole()
-    }
-
-    directives = {
-        'all': AllPostsNode,
-    }
-
-    initial_data = {
-        'objects': [],
-        'obj2ingredient': {},
-    }
-
-    def merge_domaindata(self, docnames: List[str], otherdata: Dict) -> None:
-        print(f"---------------------------> Merging domain data")
-        pass
-
-    def resolve_any_xref(self, env: "BuildEnvironment", fromdocname: str, builder: "Builder", target: str,
-                         node: pending_xref, contnode: Element) -> List[Tuple[str, Element]]:
-        print(f"---------------------------> Resolving XRef")
-        pass
+def visit_all_posts_node_html(translator: HTML5Translator, node: PostNode):
+    translator.visit_section(node)
 
 
-def setup(app: Sphinx):
+def depart_all_posts_node_html(translator: HTML5Translator, node: PostNode):
+    translator.depart_section(node)
+
+
+def visit_all_posts_node_texinfo(translator: TexinfoTranslator, node: PostNode):
+    translator.visit_section(node)
+
+
+def depart_all_posts_node_texinfo(translator: TexinfoTranslator, node: PostNode):
+    translator.depart_section(node)
+
+
+def visit_all_posts_node_text(translator: TextTranslator, node: PostNode):
+    translator.visit_section(node)
+
+
+def depart_all_posts_node_text(translator: TextTranslator, node: PostNode):
+    translator.depart_section(node)
+
+
+class AllPostsDirective(Directive):
+    def run(self) -> list[Node]:
+        return [AllPostsNode()]
+
+
+def process_post_nodes(app: Sphinx, doctree: nodes.document, fromdocname: str):
+    env = app.builder.env
+
+    if hasattr(env, "all_posts"):
+        env.all_posts = sorted(
+            env.all_posts, key=lambda post: post["date"], reverse=True
+        )
+
+    for node in doctree.traverse(AllPostsNode):
+        if not hasattr(env, "all_posts"):
+            continue
+        content = []
+        for post_meta in env.all_posts:
+            formatted_post_date = datetime.strftime(post_meta["date"], "%d %B %Y")
+
+            # Set up a section for the post content
+            post_section = nodes.section()
+            post_section.attributes["ids"] = [f"{post_meta['title'].replace(' ', '-')}"]
+
+            # Add the post title
+            title = nodes.title()
+            reference = nodes.reference("", "")
+            reference["refdocname"] = post_meta["docname"]
+            reference["refuri"] = app.builder.get_relative_uri(
+                fromdocname, post_meta["docname"]
+            )
+            reference["refuri"] += "#" + post_meta["target"]["refid"]
+            reference.append(nodes.Text(f"{post_meta['title']}"))
+            title += reference
+            post_section.append(title)
+
+            # Add date and append it to the paragraph
+            post_section += nodes.paragraph(text=f"{formatted_post_date}")
+
+            # Transition is needed for the next post
+            post_section.append(nodes.transition())
+            content.append(post_section)
+        node.replace_self(content)
+
+
+def purge_post_nodes(app: Sphinx, env: BuildEnvironment, docname: str):
+    if not hasattr(env, "all_posts"):
+        return
+    env.all_posts = [post for post in env.all_posts if post["docname"] != docname]
+
+
+def merge_post_nodes(app: Sphinx, env: BuildEnvironment, docname: str, others: list):
+    if not hasattr(env, "all_posts"):
+        env.all_posts = []
+    if hasattr(docname, "all_posts"):
+        docname.all_posts.extend(env.all_posts)
+
+
+class Metadata(TypedDict):
+    version: Optional[str]
+    parallel_read_safe: Optional[bool]
+    parallel_write_safe: Optional[bool]
+
+
+def setup(app: Sphinx) -> Metadata:
+    app.add_node(
+        AllPostsNode,
+        html=(visit_all_posts_node_html, depart_all_posts_node_html),
+        texinfo=(visit_all_posts_node_texinfo, depart_all_posts_node_texinfo),
+        text=(visit_post_node_text, depart_all_posts_node_text),
+    )
     app.add_node(
         PostNode,
         html=(visit_post_node_html, depart_post_node_html),
-        latex=(visit_post_node_texinfo, depart_post_node_texinfo),
+        texinfo=(visit_post_node_texinfo, depart_post_node_texinfo),
         text=(visit_post_node_text, depart_post_node_text),
     )
-    app.add_domain(Posts)
+    app.add_directive("all-posts", AllPostsDirective)
     app.add_directive("post", PostDirective)
+    app.connect("doctree-resolved", process_post_nodes)
+    app.connect("env-purge-doc", purge_post_nodes)
+    app.connect("env-merge-info", merge_post_nodes)
     return {
-        'version': '0.1',
-        'parallel_read_safe': True,
-        'parallel_write_safe': True,
+        "version": "0.1",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
     }
